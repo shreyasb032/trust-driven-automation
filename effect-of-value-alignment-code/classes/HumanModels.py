3c838278ca4c188f7faa54f5f14419256cc199c0
shreyasb032 <shreyas1335@gmail.com>
2023-05-18 21:13:39 -0400

    Cleaning the simulation paper code: part 1

--- effect-of-value-alignment-code/classes/HumanModels.py
+++ effect-of-value-alignment-code/classes/HumanModels.py
@@ -127,34 +127,68 @@ class HumanBase:
 
 
 class ReversePsychology(HumanBase):
-    
-    def __init__(self, params, reward_weights):
-        super().__init__(params)
-        self.reward_weights = reward_weights
+    """The reverse psychology model of human behavior"""
+
+    def __init__(self, params: List, reward_weights: Dict, reward_fun: RewardsBase,
+                 performance_metric: PerformanceMetricBase):
+        """
+        Initializes this human model
+        :param params: the trust parameters associated with the human. List [alpha0, beta0, ws, wf]
+        :param reward_weights: the reward weights associated with the human. Dict with keys 'health' and 'time'
+        :param reward_fun: the reward function associated with this human. Must have the function reward(health, time)
+        :param performance_metric: the performance metric that returns the performance given the recommendation and
+                                    outcome
+        """
+        super().__init__(params, reward_weights, reward_fun, performance_metric)
 
     def choose_action(self, rec, threat_level=None, health=None, time=None):
+        """
+        Chooses an action based on the reverse psychology model
+        :param rec: the recommendation given to the human
+        :param threat_level: a float representing the level fo threat in the current site
+        :param health: the current health level of the soldier
+        :param time: the time spent in the mission
+        """
 
         return np.random.choice([rec, 1-rec], p=[self.trust, 1-self.trust])
 
 
 class Disuse(HumanBase):
-    """Old Disuse Model: Accept recommendation with probability trust, choose the action which gives best 
+    """Old Disuse Model: Accept recommendation with probability trust, choose the action which gives the best
         immediate expected reward otherwise"""
 
-    def __init__(self, params, reward_weights, reward_fun='linear'):
-        super().__init__(params, reward_weights, reward_fun)
-    
+    def __init__(self, params: List, reward_weights: Dict, reward_fun: RewardsBase,
+                 performance_metric: PerformanceMetricBase):
+        """
+        Initializes the human base class
+        :param params: the trust parameters associated with the human. List [alpha0, beta0, ws, wf]
+        :param reward_weights: the reward weights associated with the human. Dict with keys 'health' and 'time'
+        :param reward_fun: the reward function associated with this human. Must have the function reward(health, time)
+        :param performance_metric: the performance metric that returns the performance given the recommendation and
+                                    outcome
+        """
+        super().__init__(params, reward_weights, reward_fun, performance_metric)
+
     def choose_action(self, rec, threat_level, health, time):
-        
+        """
+        Chooses an action based on the disuse model
+        :param rec: the recommendation given to the human
+        :param threat_level: a float representing the level fo threat in the current site
+        :param health: the current health level of the soldier
+        :param time: the time spent in the mission
+        """
+
         p = np.random.uniform(0, 1)
         
         # With probability = trust, choose the recommendation
         if p < self.trust:
             return rec
+
+        hl, tc = self.reward_fun.reward(health, time, house=None)
         
         # With probability = 1 - trust, choose the action that maximizes immediate expected reward
-        r0 = self.reward_weights["health"] * threat_level * self.health_loss_reward(health)
-        r1 = self.reward_weights["time"] * self.time_loss_reward(time)
+        r0 = self.reward_weights["health"] * threat_level * hl
+        r1 = self.reward_weights["time"] * tc
 
         return int(r0 < r1)
 
@@ -164,12 +198,30 @@ class BoundedRational(HumanBase):
     Chooses the action that gives the best immediate expected reward with probability proportional 
     to the exponent of that reward multiplied by the rationality coefficient"""
 
-    def __init__(self, params, reward_weights, reward_fun: RewardsBase, perf_metric: PerformanceMetricBase, kappa=1.0):
-        super().__init__(params, reward_weights, reward_fun, perf_metric)
+    def __init__(self, params, reward_weights, reward_fun: RewardsBase,
+                 performance_metric: PerformanceMetricBase,
+                 kappa=1.0):
+        """
+        Initializes the human base class
+        :param params: the trust parameters associated with the human. List [alpha0, beta0, ws, wf]
+        :param reward_weights: the reward weights associated with the human. Dict with keys 'health' and 'time'
+        :param reward_fun: the reward function associated with this human. Must have the function reward(health, time)
+        :param performance_metric: the performance metric that returns the performance given the recommendation and
+                                    outcome
+        :param kappa: the rationality coefficient for the human
+        """
+        super().__init__(params, reward_weights, reward_fun, performance_metric)
         self.kappa = kappa
 
     def choose_action(self, rec, threat_level, health, time):
-        
+        """
+        Chooses an action based on the bounded rationality disuse model
+        :param rec: the recommendation given to the human
+        :param threat_level: a float representing the level fo threat in the current site
+        :param health: the current health level of the soldier
+        :param time: the time spent in the mission
+        """
+
         p = np.random.uniform(0, 1)
         
         # With probability = trust, choose the recommendation
@@ -177,14 +229,11 @@ class BoundedRational(HumanBase):
             return rec
 
         # Else, compute the rewards for action 0 and action 1
-        h, c = self.reward_fun.reward(health, time)
-        r0 = self.reward_weights["health"] * threat_level * h
-        r1 = self.reward_weights["time"] * c
+        hl, tc = self.reward_fun.reward(health, time, house=None)
+        r0 = self.reward_weights["health"] * threat_level * hl
+        r1 = self.reward_weights["time"] * tc
 
-        p0 = np.exp(r0)
-        p1 = np.exp(r1)
-
-        p0 /= (p0+p1)
+        p0 = 1. / (1 + np.exp(r1 - r0))
 
         return np.random.choice([0, 1], p=[p0, 1-p0])
 

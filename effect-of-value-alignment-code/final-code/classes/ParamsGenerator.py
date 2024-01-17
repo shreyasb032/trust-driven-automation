99fe17d677ff131619df6cea9819f19879034e45
shreyasb032 <shreyas1335@gmail.com>
2024-01-17 15:32:38 -0500

    Added code for sampling trust parameters. TODO: integrate parameter sampling within the simulation code.

--- effect-of-value-alignment-code/final-code/classes/ParamsGenerator.py
+++ effect-of-value-alignment-code/final-code/classes/ParamsGenerator.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Dict
 
 seed = 123
 prob_bdm = 31./45.
@@ -63,23 +64,39 @@ disbeliever_hist = {
 }
 
 
+
+
 class TrustParamsGenerator:
 
     def __init__(self):
         self.rng = np.random.default_rng(seed=seed)
         self.params = None
+        self.n_bins = len(bdm_hist['Alpha']['p'])
+
+    def choose(self, data: Dict):
+        trust_params = {}
+        for key in data.keys():
+            hist_dict = data[key]
+            bin_choice = self.rng.choice(self.n_bins, hist_dict['p'])
+            bin_left, bin_right = hist_dict['bins'][bin_choice], hist_dict['bins'][bin_choice + 1]
+            trust_params[key] = self.rng.uniform(bin_left, bin_right)
+
+        return trust_params
 
     def generate(self):
         choice = self.rng.choice(3, p=[prob_bdm, prob_disbeliever, prob_oscillator])
 
         if choice == 0:
             # Bayesian decision maker
-            self.params = None
+            trust_params = self.choose(bdm_hist)
+            self.params = [trust_params['Alpha'], trust_params['Beta'], trust_params['ws'], trust_params['wf']]
         elif choice == 1:
             # Disbeliever
-            self.params = None
+            trust_params = self.choose(disbeliever_hist)
+            self.params = [trust_params['Alpha'], trust_params['Beta'], trust_params['ws'], trust_params['wf']]
         elif choice == 2:
             # Oscillator
-            self.params = None
+            trust_params = self.choose(oscillator_hist)
+            self.params = [trust_params['Alpha'], trust_params['Beta'], trust_params['ws'], trust_params['wf']]
 
-        return list(self.params)
+        return self.params
